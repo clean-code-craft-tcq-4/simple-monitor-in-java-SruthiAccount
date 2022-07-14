c1167384300e4fba80e451fe8c44dee3416459a7
Naga Sruthi Velugubantla <velugubantlasruthi@gmail.com>
2022-07-14 08:14:26 +0530

    fixes

--- vitals/GetContentInLang.java
+++ vitals/GetContentInLang.java
@@ -1,8 +1,8 @@
 package vitals;
 
 public class GetContentInLang {
-	static String getParamInLang(String param, int langIndex) {
-		if (langIndex == 1) {
+	static String getParamInLang(String param) {
+		if (Main.langIndex == 1) {
 			if (param.equalsIgnoreCase("Temperature"))
 				return "Temperatur";
 			if (param.equalsIgnoreCase("State of Charge"))
@@ -23,55 +23,55 @@ public class GetContentInLang {
 		}
 	}
 
-	static String getLowBreachInLang(int langIndex) {
-		if (langIndex == 1)
+	static String getLowBreachInLang() {
+		if (Main.langIndex == 1)
 			return " ist bei LOW BREACH !!!";
-		else if (langIndex == 0)
+		else if (Main.langIndex == 0)
 			return " is at LOW BREACH !!!";
 		else
 			return " is at LOW BREACH !!!";
 	}
 
-	static String getHighBreachInLang(int langIndex) {
-		if (langIndex == 1)
+	static String getHighBreachInLang() {
+		if (Main.langIndex == 1)
 			return " ist bei HIGH BREACH !!!";
-		else if (langIndex == 0)
+		else if (Main.langIndex == 0)
 			return " is at HIGH BREACH !!!";
 		else
 			return " is at HIGH BREACH !!!";
 	}
 
-	static String getOutOfRangeInLang(int langIndex) {
-		if (langIndex == 1)
-			return " ist auﬂer Reichweite!!!";
-		else if (langIndex == 0)
+	static String getOutOfRangeInLang() {
+		if (Main.langIndex == 1)
+			return " ist aus Angebot!!!";
+		else if (Main.langIndex == 0)
 			return " is out of range!!!";
 		else
 			return " is out of range!!!";
 	}
 
-	static String getLowWarnInLang(int langIndex) {
-		if (langIndex == 1)
+	static String getLowWarnInLang() {
+		if (Main.langIndex == 1)
 			return " ist auf NIEDRIG WARNUNG !!!";
-		else if (langIndex == 0)
+		else if (Main.langIndex == 0)
 			return " is at LOW WARNING !!!";
 		else
 			return " is at LOW WARNING !!!";
 	}
 
-	static String getHighWarnInLang(int langIndex) {
-		if (langIndex == 1)
+	static String getHighWarnInLang() {
+		if (Main.langIndex == 1)
 			return " HOCH Warnung!!!";
-		else if (langIndex == 0)
+		else if (Main.langIndex == 0)
 			return " is at HIGH WARNING !!!";
 		else
 			return " is at HIGH WARNING !!!";
 	}
 
-	static String getInRangeInLang(int langIndex) {
-		if (langIndex == 1)
+	static String getInRangeInLang() {
+		if (Main.langIndex == 1)
 			return " ist im Normalzustand";
-		else if (langIndex == 0)
+		else if (Main.langIndex == 0)
 			return " is at normal state";
 		else
 			return " is at normal state";
